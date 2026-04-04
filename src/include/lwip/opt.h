c576a569be27082b150645e8c49815484d34dac2
Ahmed Al-Jabri <ahmedaljabriedu@gmail.com>
2026-04-04 10:02:07 +0100

    Adjust TARR falg name to match the convention

--- src/include/lwip/opt.h
+++ src/include/lwip/opt.h
@@ -1500,12 +1500,12 @@
 #endif
 
 /**
- * LWIP_TARR==1: Support the TCP ACK Rate Request (TARR) Option.
+ * LWIP_TCP_TARR==1: Support the TCP ACK Rate Request (TARR) Option.
  * Implements draft-ietf-tcpm-ack-rate-request, allowing tcp sender to request
  * that the receiver change its ACK rate.
  */
-#if !defined LWIP_TARR || defined __DOXYGEN__
-#define LWIP_TARR 	   0
+#if !defined LWIP_TCP_TARR || defined __DOXYGEN__
+#define LWIP_TCP_TARR 	   0
 #endif
 
 
